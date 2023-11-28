a00e52349eb9128ea1883959a6398bbccfdbaada
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-11-28 20:06:00 +0600

    release homework 3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,30 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT c.company_name, e.first_name, e.last_name
+FROM customers c
+JOIN employees e ON c.city = 'London' AND c.company_name = e.company_name
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products p
+JOIN suppliers s ON p.discontinued = 0 AND p.units_in_stock < 25 AND p.category_id IN (1, 2)
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+WHERE customer_id NOT IN (
+    SELECT customer_id
+    FROM orders
+)
+ORDER BY company_name;
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT p.product_name
+FROM products p
+JOIN order_details od ON p.product_id = od.product_id
+WHERE od.quantity = 10;
